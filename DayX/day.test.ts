ee6be6fc94bdd510a8b2f4270e66372c8a1e821c
Rasmus Bengtsson <2124386+rasmusbe@users.noreply.github.com>
2021-12-02 11:45:20 +0100

    Output answer when test is successful

--- DayX/day.test.ts
+++ DayX/day.test.ts
@@ -3,9 +3,15 @@ import { part1, part2 } from ".";
 test("example 1", () => {
   const answer = part1(__dirname, "example.txt");
   expect(answer).toBe(0);
+
+  // Only when successful
+  console.log(`Part 1: ${part1(__dirname, "input.txt")}`);
 });
 
 // test("example 2", () => {
 //   const answer = part2(__dirname, "example.txt");
 //   expect(answer).toBe("5");
+
+//   // Only when successful
+//   console.log(`Part 2: ${part2(__dirname, "input.txt")}`);
 // });
