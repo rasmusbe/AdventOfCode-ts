81222ce5e1eda6b4dda0e1d0aa966ac44bde5350
Rasmus Bengtsson <2124386+rasmusbe@users.noreply.github.com>
2021-12-18 20:37:46 +0100

    Update template

--- DayX/index.ts
+++ DayX/index.ts
@@ -11,8 +11,3 @@ export const part2 = (dirName: string, inputFile: string): number => {
 
   return 0;
 };
-
-export const run = () => {
-  console.log("Part 1:", part1(__dirname, "input.txt"));
-  console.log("Part 2:", part2(__dirname, "input.txt"));
-};
