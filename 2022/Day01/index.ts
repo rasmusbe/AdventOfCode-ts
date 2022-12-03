515e61976304490e2bdc3e98439d78d15a3be81d
Rasmus Graham <2124386+rasmusbe@users.noreply.github.com>
2022-12-03 11:55:43 +0100

    format

--- 2022/Day01/index.ts
+++ 2022/Day01/index.ts
@@ -1,11 +1,11 @@
 import { getTextFileLines } from "../utils/getInput";
 
-const getElfCals = (input: string[]) : number[] => {
+const getElfCals = (input: string[]): number[] => {
   let elf = 0;
   const elfCals: number[] = [];
 
   input.forEach((line) => {
-        if (line.length > 0) {
+    if (line.length > 0) {
       const cals = parseInt(line);
 
       elfCals[elf] = (elfCals[elf] || 0) + cals;
@@ -15,7 +15,7 @@ const getElfCals = (input: string[]) : number[] => {
   });
 
   return elfCals;
-}
+};
 
 export const part1 = (dirName: string, inputFile: string): number => {
   const input = getTextFileLines(dirName, inputFile);
